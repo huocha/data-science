7f3cfa7ab9045345c6e0c7c3b15e0e19863c6cb0
Jasmine Ng <dnguyen@ubiqus.com>
2019-04-02 22:47:03 +0200

    update user similarity && user suggestion

--- python/similar.py
+++ python/similar.py
@@ -1,8 +1,9 @@
-from numpy import dot
+import numpy
 import math
+from collections import defaultdict
 
 def cosine_similarity(v, w):
-	return dot(v, w)/ math.sqrt( dot(v,v) * dot(w,w) )
+	return numpy.dot(v, w)/ math.sqrt( numpy.dot(v,v) * numpy.dot(w,w) )
 
 users_interests = [
 	 ["Hadoop", "Big Data", "HBase", "Java", "Spark", "Storm", "Cassandra"],
@@ -31,11 +32,38 @@ def make_user_interest_vector(user_interests):
 	return [1 if interest in user_interests else 0
 		for interest in unique_interests]
 
-# map vector of similarity vs user_interest
+# map vector of similarity vs user_interest: map(f, iterable) =  [f(x) for x in iterable]
 user_interest_matrix = list(map(make_user_interest_vector, users_interests))
 
 user_similarities = [[cosine_similarity(interest_vector_i, interest_vector_j)
 	for interest_vector_j in user_interest_matrix]
 	for interest_vector_i in user_interest_matrix]
+# print(user_similarities)
 
-print(user_similarities[0][9])
+
+# user_similarities_sorted = [ sorted(user_similarity, reverse=True) for user_similarity in user_similarities]
+# print(user_similarities_sorted)
+
+# find the most similar user to a given user
+def most_similar_users_to(user, max_result = 5):
+	user_similarities_sorted = sorted(enumerate(user_similarities[user]), key= lambda x:x[1], reverse=True)
+	return user_similarities_sorted[1: max_result+1]
+
+print(most_similar_users_to(0))
+
+# given a user_id return a list of interests he not interested yet from most to least
+def user_based_suggestion(user_id):
+	suggestions = defaultdict(float)
+	# cumulate similarity
+	for other_user, similarity in most_similar_users_to(user_id):
+		for interest in users_interests[other_user]:
+			suggestions[interest] += similarity
+	suggestions = sorted(suggestions.items(), key=lambda x:x[1], reverse=True)
+
+	return [ (suggestion, totalSimilarity)
+			for suggestion, totalSimilarity in suggestions
+			if suggestion not in users_interests[user_id] ]
+
+
+user_suggestion = user_based_suggestion(0)
+print(user_suggestion)
